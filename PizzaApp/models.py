738572a135473991fdcd85f06ede26164f94112f
anilyadav2 <anil_yadav2@baylor.edu>
2021-12-10 17:27:38 -0600

    create pizza page

--- PizzaApp/models.py
+++ PizzaApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+class Pizza_name(models.Model):
+    
+    text = models.CharField(max_length=300)
+    date_added = models.DateTimeField(
+            auto_now_add=True)
+    def __str__(self):
+        return self.text
--- PizzaApp/views.py
+++ PizzaApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
+from django.shortcuts import redirect, render
+#from .forms import EntryForm, TopicForm
+from .models import Pizza_name
 # Create your views here.
+
+def index(request):
+    return render(request,"PizzaApp/index.html")
+
+
